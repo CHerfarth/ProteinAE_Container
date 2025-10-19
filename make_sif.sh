8a82886a1f22781cc32fa8cf83e27d6f35bd239f
CHerfarth <clemens@herfarth.com>
2025-10-19 15:04:00 +0300

    Singularity setup

---
+++ make_sif.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH -t 01:00:00
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-user=herfarcf@kaust.edu.sa
+#SBATCH --mail-type=ALL
+
+module load singularity
+
+export XDG_RUNTIME_DIR=/ibex/user/herfarcf/temp
+
+singularity build --fakeroot --force ./AlphaFold.sif ./Singularity.def
